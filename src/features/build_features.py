a446ea6eb5e45276520c6d058f1122740683b611
George Mushore <gmu003@gmail.com>
2024-02-26 14:49:36 +0100

    created a util to explore data

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -26,3 +26,24 @@ def replace_value_with_grouped_mean(df, value, column, to_groupby):
     df.loc[invalid_mask, column] = means_array[invalid_mask]
 
     return df
+
+def explore_data(df):
+    """ Print out the shape of the dataframe, the first few rows, last rows and the
+        data types of the columns.
+        Describe the data frame.
+        If missing values are present, print out the percentage of missing values
+
+        :param df: The dataframe to explore
+    """
+    print(f"Shape: {df.shape}")
+    print(f"Head: {df.head()}")
+    print(f"Tail: {df.tail()}")
+    print(f"Description: {df.describe()}")
+    print(f"Data Types: {df.dtypes}")
+
+
+    missing = df.isnull().sum()
+    missing = missing[missing > 0]
+    if not missing.empty:
+        print(f"Missing Values: {missing / len(df)}")
+
